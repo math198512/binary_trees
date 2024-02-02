c43ab5e28330b12f1520c8562a57cede04d47923
math198512 <thoraichi@gmail.com>
2024-02-02 11:54:39 -0800

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,8 +14,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	{
 		if (tree->left || tree->right)
 			nodes += 1;
-		nodes += binary_tree_nodes(tree->left);
 		nodes += binary_tree_nodes(tree->right);
+		nodes += binary_tree_nodes(tree->left);
 	}
 	return (nodes);
 }
